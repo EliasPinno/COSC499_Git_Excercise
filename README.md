94d50f25c7526da7101861ded154a6ad28d3786f
EliasPinno <pucas946@gmail.com>
2022-09-16 20:17:28 -0700

    Added the mechanism and tests for sorting.
    Includes sorting for arrays, displaying the sorted array.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Individual exercise for marks in COSC 499.
 
 # First feature: Taking user input
 This project is capable of taking in user input and putting it into a list.
+
+# Second feature: sorting an array and displaying it to the user
+This project can now sort the provided input and display it to the user
--- tests.py
+++ tests.py
@@ -13,5 +13,11 @@ class TestMethods(unittest.TestCase):
     def test_BigNumbersListFromInput(self):
         self.doInputTest([111111,22,3,854],"111111 22 3 854")
 
+    def test_LSortedStandard(self):
+        self.assertCountEqual(main.sortList([287,45,360,20]),[20,45,287,360])
+
+    def test_LSortedRepeats(self):
+        self.assertCountEqual(main.sortList([45,45,10,45]),[10,45,45,45])
+
 if __name__ == '__main__':
     unittest.main()
